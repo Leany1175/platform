cd8e5d03e0f481d5185cb0fb043113316f838862
15082037343@163.com <Iloveyou+4869>
2019-02-22 17:39:52 +0800

    init

---
+++ src/main/java/com/platform/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.platform.controller;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Scope("prototype")
+public class IndexController {
+
+	@RequestMapping(value = "/")
+	public String index() {
+		return "admin/index";
+	}
+
+}
