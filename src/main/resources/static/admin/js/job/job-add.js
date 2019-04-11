f014816f88b732bb827d77a505e6bc8e7310e455
15082037343@163.com <Iloveyou+4869>
2019-04-11 20:54:35 +0800

    触发器选择

--- src/main/resources/static/admin/js/job/job-add.js
+++ src/main/resources/static/admin/js/job/job-add.js
@@ -1,8 +1,12 @@
 layui.config({
 	base: "/layui/extends/"
-}).use(["jquery", "form"], function(){
+}).use(["jquery", "form", "laydate"], function(){
 	var $ = layui.jquery
-		form = layui.form;
+		form = layui.form,
+		laydate = layui.laydate;
 	
+	laydate.render({
+		elem: "#time"
+	});
 	
 });
