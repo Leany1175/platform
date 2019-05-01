97dbc6dd0fe5371392c52c8677a2a6ffd8861053
15082037343@163.com <Iloveyou+4869>
2019-05-02 07:54:42 +0800

    other

--- src/main/java/com/platform/controller/IndexController.java
+++ src/main/java/com/platform/controller/IndexController.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 
 @Controller
 @Scope("prototype")
@@ -12,6 +16,22 @@ public class IndexController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index() {
 		return "redirect:/admin/index";
+	}
+
+	@ResponseBody
+	@RequestMapping("/data")
+	public String data(HttpServletResponse response, String path) throws IOException {
+		response.addHeader("Access-Control-Allow-Origin", "*");
+		response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(path), "UTF-8"));
+		StringBuffer buffer = new StringBuffer();
+		String line;
+		while ((line = bufferedReader.readLine()) != null) {
+			buffer.append(line);
+		}
+		return buffer.toString();
 	}
 
 }
