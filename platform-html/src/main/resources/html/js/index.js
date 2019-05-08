034e0bd9e067e81b03dba3f392682363a39c760e
15082037343@163.com <Iloveyou+4869>
2019-05-08 19:09:40 +0800

    html

---
+++ platform-html/src/main/resources/html/js/index.js
@@ -0,0 +1,114 @@
+layui.use(["jquery"], function() {
+	var $ = layui.jquery;
+	
+// 	// 开始拖动元素时触发
+// 	document.addEventListener("dragstart", function(
+// 		console.log(event.target);
+// 		event.dataTransfer.effectAllowed = "all";
+// 		// console.log("dragstart");
+// 	}, false);
+// 	
+// 	// 用户完成元素拖动后触发
+// 	document.addEventListener("dragend", function() {
+// 		// console.log("dragend");
+// 	}, false);
+// 	
+// 	// 拖动时
+// 	document.addEventListener("drag", function(event) {
+// 		// event.preventDefault();
+// 		event.dataTransfer.dropEffect = "none";
+// 		// console.log(event);
+// 		event.dataTransfer.effectAllowed = "none";
+// 	}, false);
+// 
+// 	// 当被鼠标拖动的对象进入其容器范围内时触发此事件
+// 	document.addEventListener("dragenter", function(event) {
+// 		// console.log("dragenter");
+// 		console.log(event);
+// 		if ("canvas" == event.target.id) {
+// 			event.dataTransfer.dropEffect = "none";
+// 			// event.dataTransfer.effectAllowed = "copy";
+// 			console.log("canvas")
+// 		} else {
+// 			event.dataTransfer.dropEffect = "none";
+// 		}
+// 	}, false);
+// 	
+// 	// 当某被拖动的对象在另一对象容器范围内拖动时触发此事件
+// 	document.addEventListener("dragover", function(event) {
+// 		event.preventDefault();
+// 		// console.log("dragover");
+// 	}, false);
+// 	
+// 	//  当被鼠标拖动的对象离开其容器范围内时触发此事件
+// 	document.addEventListener("dragleave", function() {
+// 		// console.log("dragleave");
+// 		if ("canvas" == event.target.id) {
+// 			event.dataTransfer.dropEffect = "none";
+// 			event.dataTransfer.effectAllowed = "none";
+// 		}
+// 	}, false);
+
+// 	var charts = document.getElementsByClassName("chart");
+// 	for (var i = 0; i < charts.length; i++) {
+// 		
+// 		charts[i].ondragstart = function(event) {
+// 			event.dataTransfer.effectAllowed = "copy";
+// 			event.dataTransfer.dropEffect = "copy";
+// 			console.log(event.target)
+// 			console.log("dragstart");
+// 		}
+// 	}
+// 	
+// 	// 目标容器
+// 	var canvas = document.getElementById("canvas");
+// 	
+// 	canvas.ondragenter = function(event) {
+// 		event.dataTransfer.dropEffect = "copy";
+// 		event.dataTransfer.effectAllowed = "copy";
+// 		console.log("ondragenter");
+// 	}
+
+
+	// 目标容器
+	var target = document.getElementById("target");
+	// 源容器
+	var sources = document.getElementsByClassName("chart");
+	
+	for (var i = 0; i < sources.length; i++) {
+		// 开始
+		sources[i].ondragstart = function(event) {
+			console.log("ondragstart")
+		}
+		// 结束
+		sources[i].ondragend = function() {
+			console.log("dragend");
+		}
+	}
+	
+	// 在目标容器上拖动
+	target.ondragover = function(event) {
+		console.log("ondragover");
+		event.preventDefault();
+	}
+	
+	// 进入目标容器
+	target.ondragenter = function(event) {
+		console.log("ondragenter");
+		// 复制
+		event.dataTransfer.dropEffect = "copy";
+	}
+// 	target.ondragleave = function() {
+// 		console.log("ondragleave")
+// 	}
+// 	target.ondrop = function(event) {
+// 		console.log("ondrop");
+// 	}
+
+	
+	// console.log(charts.length);
+
+
+
+
+});
